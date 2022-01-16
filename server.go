c183c3d69ef26239049ef971b5e4af8e5aa7af21
cmarcelo963 <cmarcelo963@yahoo.com>
2022-01-16 17:01:02 +0000

    Add css

--- server.go
+++ server.go
@@ -3,6 +3,8 @@ package forum
 import "net/http"
 
 func Server() {
+	http.Handle("/index.css", http.FileServer(http.Dir("../static")))
 	http.HandleFunc("/", HandleDefault)
-	http.ListenAndServe(":8080", nil)	
+	http.ListenAndServe(":8080", nil)
+
 }
