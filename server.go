82a079b7b9fb3da67a251b542bfe2c0bfa1c76c8
estlop <lopezesteban8@gmail.com>
2022-01-19 14:04:20 +0000

    Implement changes to render home icon

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ func Server() {
 	http.Handle("/index.css", http.FileServer(http.Dir("../static")))
 	http.Handle("/search.svg", http.FileServer(http.Dir("../static/img")))
 	http.Handle("/calendar.svg", http.FileServer(http.Dir("../static/img")))
+	http.Handle("/home.svg", http.FileServer(http.Dir("../static/img")))
 	http.HandleFunc("/", HandleDefault)
 	http.ListenAndServe(":8080", nil)
-
 }
