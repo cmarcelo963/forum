a486afba6c836d62c7979c621ad144aad5438917
estlop <lopezesteban8@gmail.com>
2022-02-08 19:57:36 +0000

    Create placeholder that only shows when user is authenticated

--- handledefault.go
+++ handledefault.go
@@ -2,14 +2,30 @@ package forum
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
-
+type Auth struct {
+	Authenticated string
+}
 func HandleDefault(w http.ResponseWriter, r *http.Request) {
 	tpl, err := template.ParseFiles("../static/templates/index.gohtml")
 	if err != nil {
 		fmt.Println(err)
 	}
-	tpl.Execute(w, nil)
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			tpl.Execute(w, nil)
+			return
+		}
+		// For any other type of error, return a bad request status
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Println("Session token: ", c.Value)
+	var userSession Auth
+	userSession.Authenticated = "authenticated"
+	tpl.Execute(w, userSession)
 }
