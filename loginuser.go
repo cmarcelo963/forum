1bc222100c70376eb1aedd87f30e37b7ac6ab56f
karyun.cheung <karyun.cheung@gmail.com>
2022-02-12 13:05:28 +0000

    Partial encryption solution

--- loginuser.go
+++ loginuser.go
@@ -11,24 +11,37 @@ func LoginUser(userData []string, sessionToken string) bool {
 		log.Println(err.Error())
 	}
 	defer forumDatabase.Close()
-	loginSQL := `SELECT username, password FROM user WHERE username = ? AND password = ?`
-	row := forumDatabase.QueryRow(loginSQL, userData[0], userData[1])
+	loginSQL := `SELECT password FROM user WHERE username = ?`
+	row := forumDatabase.QueryRow(loginSQL, userData[0])
+	log.Println(row)
 	var userName string
 	var password string
-	err = row.Scan(&userName, &password)
-	switch err {
-	case sql.ErrNoRows:
-		log.Println("Username or password incorrect!")
-		return false
-	case nil:
+	row.Scan(&userName, &password)
+	checkPwdMatch := ComparePasswords(password, []byte(userData[1]))
+	log.Println("password: ", userData[1])
+	log.Println("Hashed Password: ", password)
+
+	if checkPwdMatch {
 		log.Println("Logged in succesfully: ", userName, password)
-		log.Println("hi")
 		CreateSession(userName, sessionToken)
 		return true
-	default:
-		log.Println(err.Error())
+	} else {
+		log.Println("Username or password incorrect!")
 		return false
 	}
+	// switch err {
+	// case sql.ErrNoRows:
+	// 	log.Println("Username or password incorrect!")
+	// 	return false
+	// case nil:
+	// 	log.Println("Logged in succesfully: ", userName, password)
+	// 	log.Println("hi")
+	// 	CreateSession(userName, sessionToken)
+	// 	return true
+	// default:
+	// 	log.Println(err.Error())
+	// 	return false
+	// }
 
 	//displayUsers(forumDatabase)
 }
