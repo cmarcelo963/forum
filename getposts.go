6aad10e2386b79c064e693c2efa2206aeb31700d
estlop <lopezesteban8@gmail.com>
2022-02-22 16:07:14 +0000

    Implement code to get post data from database

--- getposts.go
+++ getposts.go
@@ -4,7 +4,14 @@ import (
 	"database/sql"
 	"log"
 )
-
+type Post struct {
+	PostId string
+	Title string
+	Content string
+	Username string
+	Date string
+	Categories string
+}
 func GetPosts(category string) {
 	forumDatabase, err := sql.Open("sqlite3", "./forum-database.db")
 	if err != nil {
@@ -16,9 +23,20 @@ func GetPosts(category string) {
 	if err != nil {
 		log.Println(err.Error())
 	}
-	filteredPosts, err := statement.Exec(category)
+	category = "%" + category + "%"
+	filteredPostsRows, err := statement.Query(category)
+	var filteredPosts []Post
+	for filteredPostsRows.Next() {
+		var p Post
+		if err := filteredPostsRows.Scan(&p.PostId, &p.Title, &p.Content,&p.Username, &p.Date, &p.Categories); err != nil {
+			log.Println(err.Error())
+			break
+		}
+		filteredPosts = append(filteredPosts, p)
+	}
+
 	if err != nil {
 		log.Println(err.Error())
 	}
-	log.Println("THIS >", filteredPosts.)
+	log.Println("THIS >", filteredPosts, category)
 }
