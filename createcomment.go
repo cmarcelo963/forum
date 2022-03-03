9a36fed2d02d12ddcb2723f79b1f65c15f7bddd4
karyun.cheung <karyun.cheung@gmail.com>
2022-03-03 20:37:47 +0000

    Create comment table with sql query

---
+++ createcomment.go
@@ -0,0 +1,23 @@
+package forum
+
+import (
+	"database/sql"
+	"log"
+)
+
+func createCommentTable(db *sql.DB) {
+	createCommentTableSQL := `
+		CREATE TABLE IF NOT EXISTS comment(
+		"comment_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"post_id" integer,
+		"content" TEXT,
+		"username" TEXT,
+		"created_date" DATETIME,
+	 );
+	 `
+	statement, err := db.Prepare(createCommentTableSQL)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	statement.Exec()
+}
